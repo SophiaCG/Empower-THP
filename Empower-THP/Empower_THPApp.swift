1648109112ec4a1aa31f7e7fb0ca5634e39527f3
SophiaCG <sophia.gorgonio@gmail.com>
2024-03-07 14:43:15 -0500

    Initial commit

---
+++ Empower-THP/Empower_THPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Empower_THPApp.swift
+//  Empower-THP
+//
+//  Created by Sophia Gorgonio on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Empower_THPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
